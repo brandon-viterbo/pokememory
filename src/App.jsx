ce9fcd290fab9c02abf3e36df8c9d449b01b022d
brandon-viterbo <brandonjviterbo@gmail.com>
2024-11-25 00:06:57 -0500

    Made gameboard update score and best states properly following clicks

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,51 @@ import fetchRandomArrOfPokemon from './fetchRandonArrOfPokemon'
 
 let didInit = false;
 
+function shuffleArray(arr) {
+  let shuffled = arr
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+
+  return shuffled;
+}
+
 function App() {
   const [pokemon, setPokemon] = useState([]);
-
-  function handleScore() {
-
-  }
-
+  const [score, setScore] = useState(0);
+  const [best, setBest] = useState(0);
+  
   useEffect(() => {
     if (!didInit) {
       didInit = true;
       fetchRandomArrOfPokemon(12)
-        .then(response => {setPokemon(response)}); 
+      .then(response => {setPokemon(response)}); 
     }
   }, []);
 
+  if (score > best) {
+    setBest(score);
+  }
+  
+  function handleScore(e) {
+    const target = e.currentTarget;
+
+    if (target.className === 'unclicked') {
+      setScore(score + 1);
+      target.className = 'clicked'
+    } else if (target.className === 'clicked') {
+      setScore(0);
+
+      Array.from(target.parentElement.children)
+        .map(card => card.className = 'unclicked');
+    }
+
+    setPokemon(shuffleArray(pokemon));
+  }
+
   return (
     <>
-      <Header />
+      <Header score={score} best={best} />
       <Gameboard pokemon={pokemon} handleScore={handleScore} />
     </>
   )
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,17 +1,17 @@
+import '../styles/Gameboard.css'
+
 function Card({pokemon}) {
   return (
-    <>
-      <p>
-        <img src={pokemon.imageUrl} alt='' />
-        <strong>{pokemon.name}</strong>
-      </p>
-    </>
+    <p className='card'>
+      <img src={pokemon.imageUrl} alt='' />
+      <strong>{pokemon.name}</strong>
+    </p>
   )
 }
 
 function Gameboard({pokemon, handleScore}) {
-  const cards = pokemon.map((thisPokemon) =>
-    <li className='card' key={thisPokemon.id} onClick={handleScore}>
+  let cards = pokemon.map((thisPokemon) =>
+    <li className="unclicked" key={thisPokemon.id} onClick={handleScore}>
       <Card pokemon={thisPokemon} />
     </li>
   );
